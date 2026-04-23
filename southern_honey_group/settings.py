00e572c019c253b31b27e4dcc53f84034d82d775
Bautista Ribotta <bautistaribotta@hotmail.com>
2026-04-23 19:17:30 -0300

    Quito el modo DEBUG y cambio el host a localhost

--- southern_honey_group/settings.py
+++ southern_honey_group/settings.py
@@ -8,7 +8,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
@@ -18,7 +17,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['bautistaribotta.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
 
 # Application definition
