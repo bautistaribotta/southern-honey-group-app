6d1b1c1d26dee654b1d375dbd411390e3fea1eb2
Bautista Ribotta <bautistaribotta@hotmail.com>
2026-05-01 03:50:58 -0300

    Corrijo pequeño bug con respecto a la ruta de los archivos static

--- southern_honey_group/settings.py
+++ southern_honey_group/settings.py
@@ -107,7 +107,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Sirve para que sin importar desde donde se acceda
 # lleve a la url de inicio
