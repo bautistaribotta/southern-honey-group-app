aceaa6c5578118a4a7bc3b1013ca3fa6dd55f8ac
Bautista Ribotta <bautistaribotta@hotmail.com>
2026-05-19 14:34:11 -0300

    Vuelvo a agregar los imports de models.py en view.py

--- main/views.py
+++ main/views.py
@@ -9,6 +9,8 @@ from django.core.paginator import Paginator
 from django.contrib import messages
 from django.http import JsonResponse, HttpResponse
 from django.db import transaction
+
+from .models import Cliente, Producto, Operacion, DetalleOperacion, Pago, Cotizaciones, Chofer, Vehiculo, Viaje
 from .pdf_services import Remito
 from .services import (
     nuevo_producto,
